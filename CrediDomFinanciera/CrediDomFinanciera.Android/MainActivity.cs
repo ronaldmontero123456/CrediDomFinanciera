7f3ce87721282f132c40364812840350090edfed
Ronald Montero <r.montero@gruporamos.com>
2023-03-13 07:53:34 -0400

    ...

--- CrediDomFinanciera/CrediDomFinanciera.Android/MainActivity.cs
+++ CrediDomFinanciera/CrediDomFinanciera.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.Views;
 
 namespace CrediDomFinanciera.Droid
 {
-    [Activity(Label = "CrediDom", Icon = "@mipmap/CrediDom", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "credidomaplicacion", Icon = "@mipmap/credidomaplicacion", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
